332b35d1153d50e619c1fec7193bcf0d58afc16e
Charles Lavery <clavery@salesforce.com>
2022-01-25 21:04:13 -0500

    wip

---
+++ README.md
@@ -0,0 +1,3 @@
+# b2c-tools Demo
+
+Demonstration project for b2c-tools (https://github.com/SalesforceCommerceCloud/b2c-tools)
---
+++ migrations/_bootstrap_ensurePermission.js
@@ -0,0 +1,10 @@
+/**
+ * Adds additional permissions required by this project
+ *
+ * @param {Environment} env
+ * @param {Logger} logger
+ * @param {MigrationHelpers} helpers
+ * @return {Promise<void>}
+ */
+module.exports = async function ({env, logger, helpers}) {
+}
