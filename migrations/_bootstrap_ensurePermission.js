fc05f9410430dfe48c182da216263caff72952d9
Charles Lavery <clavery@salesforce.com>
2022-01-25 21:38:06 -0500

    perm boostrap

--- migrations/_bootstrap_ensurePermission.js
+++ migrations/_bootstrap_ensurePermission.js
@@ -7,4 +7,48 @@
  * @return {Promise<void>}
  */
 module.exports = async function ({env, logger, helpers}) {
+    const {ensureDataAPIPermissions} = helpers;
+
+    await ensureDataAPIPermissions(env, REQUIRED_RESOURCES, async () => {
+        // check that we can read sites
+        var sites = await env.ocapi.get('sites');
+        var firstSiteID = sites.data.data[0].id
+        await env.ocapi.get(`sites/${firstSiteID}`);
+        return true;
+    });
 }
+
+const REQUIRED_RESOURCES = [
+    {
+        'methods': [
+            'get'
+        ],
+        'read_attributes': '(**)',
+        'resource_id': '/sites'
+    },
+    {
+        'methods': [
+            'get',
+            'put',
+            'post',
+            'delete',
+            'patch'
+        ],
+        'read_attributes': '(**)',
+        'resource_id': '/sites/**'
+    },
+    {
+        'methods': [
+            'get'
+        ],
+        'read_attributes': '(**)',
+        'resource_id': '/catalogs'
+    },
+    {
+        'methods': [
+            'get'
+        ],
+        'read_attributes': '(**)',
+        'resource_id': '/inventory_lists'
+    }
+];
